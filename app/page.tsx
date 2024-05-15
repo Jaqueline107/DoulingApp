b011c02fd3c4df134c31034481f6a7542322ff95
jaqueline107 <jaquelinemouradoli@gmail.com>
2024-05-15 20:18:12 -0300

    create components buttons and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
- return (
-  <div>
-    <p className="text-green-500 font-bold text">Hello Lingo!</p>
-    <Button variant="outline">Button</Button>
-  </div>
- )
+  return (
+    <div>
+      <p className="text-green-500 font-bold text">Hello Lingo!</p>
+    </div>
+  );
 }
