e615f70330d5f59b90d44050bd173ba8d52b6439
jaqueline107 <jaquelinemouradoli@gmail.com>
2024-05-27 10:14:44 -0300

    create footer and header in maketing

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import elephant from '../../assets/douling.png';
+import Logo from '../../assets/douling.png';
 import {
   ClerkLoaded,
   ClerkLoading,
@@ -18,7 +18,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image src={elephant} width={'40'} height={'40'} alt={'e'} />
+          <Image src={Logo} width={'40'} height={'40'} alt={'logo'} />
           <h1 className="text-2xl font-extrabold text-lime-500 tracking-wide">
             Doulingo
           </h1>
